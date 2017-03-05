1184ef75c7e4ec77bbcd3d7f33875ac87ff996a2
PATIAT <paul.trotter9@gmail.com>
2017-03-05 20:14:00 +0000

    controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Angular Todo!";
+        $scope.title = "Welcome to your todo list Paul...";
     });
